35c4626cc7c15b216e4575919901cafd576b120f
Ilusha <120932005+IlushaSPB@users.noreply.github.com>
2023-04-04 10:23:40 +0300

    Create Drop.sql

---
+++ Drop.sql
@@ -0,0 +1,18 @@
+DROP TABLe award CASCADE;
+DROP TABLE competition CASCADE;
+DROP TABLE human CASCADE;
+DROP TABLE information CASCADE;
+DROP TABLE result CASCADE;
+DROP TABLE take_a_part CASCADE;
+DROP TABLE total CASCADE;
+DROP TABLE track CASCADE;
+DROP TABLE traking CASCADE;
+DROP TABLE type_track CASCADE;
+DROP FUNCTION check_org;
+DROP FUNCTION check_p;
+DROP FUNCTION check_s;
+DROP FUNCTION check_sport;
+DROP PROCEDURE add_human;
+DROP PROCEDURE add_result;
+DROP FUNCTION show_all_sportsmen;
+DROP FUNCTION show_all_tracks;
